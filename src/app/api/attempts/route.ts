af185738882f4c697f06c0af9287b83c55be17f6
lsearle94 <laura-jean94@hotmail.com>
2025-10-04 10:27:07 +1000

    Added PUT and DELETE for CRUD functionality

--- src/app/api/attempts/route.ts
+++ src/app/api/attempts/route.ts
@@ -3,6 +3,7 @@ import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+//GET
 export async function GET() {
     try {
         const attempts = await prisma.escapeAttempt.findMany({
@@ -15,6 +16,7 @@ export async function GET() {
     }
 }
 
+//POST
 export async function POST(req: Request) {
   try {
     const body = await req.json();
@@ -29,5 +31,38 @@ export async function POST(req: Request) {
   } catch (error) {
     console.error("Prisma POST Error:", error);
     return NextResponse.json({ error: "Failed to save attempt" }, { status: 500 });
+  }
+}
+
+//PUT
+export async function PUT(req: Request) {
+  try {
+    const body = await req.json();
+    const updateAttempt = await prisma.escapeAttempt.update({
+      where: {id: body.id},
+      data: {
+        player: body.player,
+        timeTaken: body.timeTaken,
+        success: body.success,
+      },
+    });
+    return NextResponse.json(updateAttempt);
+  } catch (error) {
+    console.error('Prisma PUT error:', error);
+    return NextResponse.json({ error: 'Failed to update attempt' }, {status: 500});
+  }
+}
+
+//DELETE
+export async function DELETE(req: Request) {
+  try {
+    const body = await req.json();
+    await prisma.escapeAttempt.delete({
+      where: {id: body.id},
+    });
+    return NextResponse.json({ message: 'Attempt deleted successfully' });
+  } catch (error) {
+    console.error('Prisma DELETE error:', error);
+    return NextResponse.json({error: 'Failed to delete attempt' }, {status: 500});
   }
 }
